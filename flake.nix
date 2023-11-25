91c580f047b74bb7bc75ac6e9adb73e3a20c2256
Steffen Vogel <post@steffenvogel.de>
2023-11-25 14:07:46 +0100

    Add Nix flake and direnv config
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              pkg-config
+              clang
+              go_1_21
+            ]
+            ++ lib.optionals pkgs.stdenv.isLinux [
+              pcsclite
+              pcsctools
+            ];
+        };
+
+        formatter = nixpkgs.alejandra;
+      }
+    );
+}
